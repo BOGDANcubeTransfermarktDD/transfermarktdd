1918f69209cfb84858a5ec21558a66000f77138e
BOGDANcubeTransfermarktDD <bogdancube2020@mail.ru>
2026-06-11 21:34:15 +0400

    Add files via upload
    Обновление с "Новости" и "Рейтинги"

---
+++ players-data.js
@@ -0,0 +1,96 @@
+const playersData = [
+    {
+        id: "bogdan",
+        name: "Богдан",
+        age: 16,
+        price: 65,
+        position: "forward",
+        positions: ["Нападающий", "Вратарь"],
+        mainPosition: "Нападающий",
+        number: 12,
+        club: "ФК ПАЦАНЧИКИ",
+        nation: "Россия",
+        height: 176,
+        photo: "https://i.oneme.ru/i?r=BTE2sh_eZW7g8kugOdIm2NotuvU1F-EcpDkUCT7EvWbaTqi4PqKgM2Ek2hqD_lAdUA8",
+        stats: {
+            matches: 2,
+            goals: 5,
+            assists: 4,
+            yellowCards: 0,
+            redCards: 0
+        },
+        goalieStats: {
+            matches: 1,
+            conceded: 2,
+            cleanSheets: 0,
+            yellowCards: 0,
+            redCards: 0
+        }
+    },
+    {
+        id: "vlad",
+        name: "Влад",
+        age: 15,
+        price: 65,
+        position: "forward",
+        positions: ["Нападающий"],
+        mainPosition: "Нападающий",
+        number: 10,
+        club: "ФК ПАЦАНЧИКИ",
+        nation: "Россия",
+        height: 175,
+        photo: "https://i.oneme.ru/i?r=BTE2sh_eZW7g8kugOdIm2NotMRmAJgu4rI1Sy6GhWyDp_ai4PqKgM2Ek2hqD_lAdUA8",
+        stats: {
+            matches: 2,
+            goals: 8,
+            assists: 6,
+            yellowCards: 0,
+            redCards: 0
+        },
+        goalieStats: null
+    },
+    {
+        id: "danila",
+        name: "Данила",
+        age: 15,
+        price: 65,
+        position: "forward",
+        positions: ["Нападающий"],
+        mainPosition: "Нападающий",
+        number: 11,
+        club: "ФК ПАЦАНЧИКИ",
+        nation: "Россия",
+        height: 186,
+        photo: "https://i.oneme.ru/i?r=BTE2sh_eZW7g8kugOdIm2NotwWU8xPPEixe5q4cGZEllb6i4PqKgM2Ek2hqD_lAdUA8",
+        stats: {
+            matches: 2,
+            goals: 7,
+            assists: 4,
+            yellowCards: 0,
+            redCards: 0
+        },
+        goalieStats: null
+    },
+    {
+        id: "boris",
+        name: "Борис Лов Ельцин",
+        age: 8,
+        price: 55,
+        position: "midfielder",
+        positions: ["Полузащитник"],
+        mainPosition: "Полузащитник",
+        number: 8,
+        club: "ФК ПАЦАНЧИКИ",
+        nation: "Россия",
+        height: 140,
+        photo: "https://i.oneme.ru/i?r=BTE2sh_eZW7g8kugOdIm2NotQSRBqBAQlyOG-Qap9IFDaKi4PqKgM2Ek2hqD_lAdUA8",
+        stats: {
+            matches: 1,
+            goals: 1,
+            assists: 4,
+            yellowCards: 0,
+            redCards: 0
+        },
+        goalieStats: null
+    }
+];
